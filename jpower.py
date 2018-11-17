b5d343c47165dc70604545be577578ec411ee26b
ljurk <ljurk@pm.me>
2018-11-17 16:20:59 +0100

    add recursive printing of json

--- jpower.py
+++ jpower.py
@@ -8,40 +8,57 @@ data = {}
 args = sys.argv
 args.pop(0)
 foundString=''
+recursive =0
+
+#show recursive
+def formatData(t,s):
+    if not isinstance(t,dict) and not isinstance(t,list):
+        print("- "*s+str(t) + ' *e*')
+    else:
+        for key in t:
+            print("- "*s+str(key))
+            if not isinstance(t,list):
+                formatData(t[key],s+1)
 
 #open file
 with open(sourceFilePath, 'r') as read_file:
     data = json.load(read_file)
 
+#parse arguments 
 for i in range(len(args)):
-    if args[i].startswith('-'):
+    if args[i].startswith('--'):
+        if args[i] == '--r':
+            recursive = 1
+            args.pop(i)
+    elif args[i].startswith('-'):
         args[i] = args[i].replace('-','',1)
     else:
         setTo = args[i]
         args.pop(i)
 
-argCount = len(args)
 print(sourceFilePath)
 print(args)
-print('depth: ' + str(argCount))
+print('depth: ' + str(len(args)))
 print()
 
 #show objects under given path
 elem=data
-for i in range(argCount):
+for i in range(len(args)):
     elem=elem[args[i]]
 
-for e in elem:
-    if len(e) == 1:
-        foundString = True
-    else:
+if recursive:
+    #recursive
+    formatData(elem,0)
+else:
+    #only actual level
+    for e in elem:
         print(e)
 
 if foundString:
-    print(elem)
+    print(elem + ' *e*')
     if setTo != '':
         #change data to input
-        if argCount == 3:
+        if len(args) == 3:
             print('change ' + data[args[0]][args[1]][args[2]] + ' to ' + setTo)
             #backup actual config
             os.system('cp ' + sourceFilePath + ' ' + sourceFilePath + '.old')
